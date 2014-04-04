bc08e7a7a46b9c15e7a8761b7aa3f23704f2f721
Jveenstra1988 <jveenstra1988@gmail.com>
2014-04-04 13:03:20 +0200

    ;km

--- index.php
+++ index.php
@@ -175,10 +175,13 @@
             </p>
             
 <<<<<<< HEAD
+<<<<<<< HEAD
             <span class="afbeelding">
             <img src="img/works/sto/jb-store1.jpg"><img src="img/works/sto/jb-store2.jpg"><img src="img/works/sto/jb-store3.jpg"><img src="img/works/sto/jb-store4.jpg">
             </span>
             
+=======
+>>>>>>> 5fd56f44b1deb777ca1b9fe138c3a4b47ed25448
 =======
 >>>>>>> 5fd56f44b1deb777ca1b9fe138c3a4b47ed25448
             <a href="#" class="btn-rood a-btn" id="work-load">
