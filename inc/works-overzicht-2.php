ceb145c1d6fbe4c805f8ea7f4d45778fbedc6c20
Jveenstra1988 <jveenstra1988@gmail.com>
2014-06-09 13:17:16 +0200

    lkn

--- inc/works-overzicht-2.php
+++ inc/works-overzicht-2.php
@@ -1,78 +1,28 @@
 <!-- Projectpagina 2 -->
 <section id="works-overzicht" class="">
-	<a href="#" class="btn-rood back">&#8592; Terug</a>
-	<article class="bg-grijs2 images">
-		<header>
-			<h1 class="txt-zwart txt-left">Projecten</h1>
-						<span class="pager">
-				<a href='#' class='btn-rood' id='page-one'>1</a>
-				<a href='#' class='btn-rood active' id='page-two'>2</a>
-			</span>
-		</header>
-		<a href="#" class="btn-wit stroke active" id="all">Projecten</a>
-		<a href="#" class="btn-wit stroke" id="stores">Stores</a>
-		<a href="#" class="btn-wit stroke" id="commercials">Commercials</a>
-		<a href="#" class="btn-wit stroke" id="interiors">Interieurs</a>
-		<a href="#" class="btn-wit stroke" id="horeca">Horeca</a>
-		<a href="#" class="btn-wit stroke" id="stands">Stands</a>
-		<a href="#" class="btn-wit stroke" id="designs">Designs</a>
+        <a href="#" class="btn-rood back">&#8592; Terug</a>
+        <article class="bg-grijs2 images">
+                <header>
+                        <h1 class="txt-zwart txt-left">Projecten</h1>
+                                                <span class="pager">
+                                <a href='#' class='btn-rood' id='page-one'>1</a>
+                                <a href='#' class='btn-rood active' id='page-two'>2</a>
+                        </span>
+                </header>
+                <a href="#" class="btn-wit stroke active" id="all">Projecten</a>
+                <a href="#" class="btn-wit stroke" id="stores">Stores</a>
+                <a href="#" class="btn-wit stroke" id="commercials">Commercials</a>
+                <a href="#" class="btn-wit stroke" id="interiors">Interieurs</a>
+                <a href="#" class="btn-wit stroke" id="horeca">Horeca</a>
+                <a href="#" class="btn-wit stroke" id="stands">Stands</a>
+                <a href="#" class="btn-wit stroke" id="designs">Designs</a>
 
-		
-		<div id="results" class="imageholder">
-			
-			<div class="clickable horeca">
-				<h3 class="overlay-txt" id="koning">de Koning</h3>
-				<img src="img/works/hor/color/koning6.jpg" >
-				<div class="overlay" id="koning">
-				</div>
-			</div>
-			
-			<div class="clickable interiors">
-				<h3 class="overlay-txt" id="koopman">Koopman Optiek</h3>
-				<img src="img/works/sto/color/koopman1.jpg" >
-				<div class="overlay" id="koopman">
-				</div>
-			</div>
-			
-			<div class="clickable commercials">
-				<h3 class="overlay-txt" id="bewuzt">Bewuzt</h3>
-				<img src="img/works/com/color/bewuzt1.jpg" >
-				<div class="overlay" id="bewuzt">
-				</div>
-			</div>
+                
+                <div id="results" class="imageholder">
 
-			<div class="clickable horeca">
-				<h3 class="overlay-txt" id="peters">Peter's Irish Pub</h3>
-				<img src="img/works/hor/color/peters6.jpg" >
-				<div class="overlay" id="peters">
-				</div>
-			</div>
-			
-			<div class="clickable designs">
-				<h3 class="overlay-txt" id="jb-design">Just Brands Design</h3>
-				<img src="img/works/des/color/jb-design1.jpg" >
-				<div class="overlay" id="jb-design">
-				</div>
-			</div>
 
-			<div class="clickable stands">
-				<h3 class="overlay-txt" id="modefabriek">Modefabriek Amsterdam</h3>
-				<img src="img/works/std/color/modefabriek7.jpg" >
-				<div class="overlay" id="modefabriek"></div>
-			</div>
+<!-- HIER KUNNEN DE PROJECTEN GEPLAATST WORDEN -->
 
-			<div class="clickable commercials">
-				<h3 class="overlay-txt" id="ziggo">Ziggo</h3>
-				<img src="img/works/com/color/ziggo3.jpg" >
-				<div class="overlay" id="ziggo"></div>
-			</div>
-
-			<div class="clickable stores">
-				<h3 class="overlay-txt" id="friends-emmen">Friends Emmen</h3>
-				<img src="img/works/sto/color/friends1.jpg" >
-				<div class="overlay" id="friends-emmen"></div>
-			</div>
-
-		</div>
-	</article>
+                </div>
+        </article>
 </section>
