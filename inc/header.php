5a28e37692691cf4d6713d8b9183fc44eaa5c9ce
jeroenklaver <jeroenklaver1987@gmail.com>
2014-06-09 14:03:37 +0200

    whole new BACKUP

---
+++ inc/header.php
@@ -0,0 +1,58 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Edit-All</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+		<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/idangerous.swiper.css">
+		<link rel="stylesheet" type="text/css" href="css/slicebox.css" />
+		<link href="css/lightbox.css" rel="stylesheet" />
+		<link rel="stylesheet" type="text/css" href="css/jquery.fancybox.css" media="screen" />
+		
+		<script src="js/vendor/jquery-1.10.1.min.js"></script>
+        <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+		<script type="text/javascript" src="//use.typekit.net/led7cxt.js"></script>
+
+		<!-- Font-face -->
+		<link rel="stylesheet" href="css/fonts.css" type="text/css" charset="utf-8" />
+
+		<!-- Typekit font -->
+		<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+		<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBj5-QRLtxngjfwUMk_byK4EBpwIJxK6uM&sensor=false"></script>
+	</head>
+	<body>
+		<!--[if lt IE 7]>
+			<p class="chromeframe">Je gebruikt een <strong>sterk verouderde</strong> browser. <a href="http://browsehappy.com/">Upgrade je browser</a> of <a href="http://www.google.com/chromeframe/?redirect=true">activeer Google Chrome Frame</a> om uw webervaring te verbeteren.</p>
+		<![endif]-->
+		<nav class="bg-wit bottom">
+			<ul class="br-wit">
+				<li>
+					<a href="" id="home" class="active">Edit-All</a>
+				</li>
+				<li>
+					<a href=""id="works">Projecten</a>
+				</li>
+				<li class="home">
+					 <a href="" id="home" class="">
+						<img src="img/beeldmerk.png" class="beeldmerk in-nav">  
+					 </a>
+				</li>
+				<li>
+					<a href="" id="profile">Profiel</a>
+				</li>
+				<li>
+					<a href="" id="contact">Contact</a>
+				</li>
+			</ul>
+		</nav>
