51591140d44dd84a81762ae74e1a7aab69367f09
Jveenstra1988 <jveenstra1988@gmail.com>
2014-06-11 20:46:20 +0200

    Opschonen en commentaar toevoegen main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,4 @@
-/**
- * jQuery.browser.mobile (http://detectmobilebrowser.com/)
- *
- * jQuery.browser.mobile will be true if the browser is a mobile device
- *
- **/
+//jQuery.browser.mobile will be true if the browser is a mobile device
 (function(a){(jQuery.browser=jQuery.browser||{}).mobile=/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))})(navigator.userAgent||navigator.vendor||window.opera);
 
 $(document).ready(function(){
@@ -18,6 +13,7 @@ $(document).ready(function(){
 		$('a, img, body, #een, #twee, #drie, #vier, #vijf, #zes, #works-container, #profile-container').removeClass('hires');
 	};
 	
+	//	Change content of next & prev projectbuttons
 	if ( ($(window).width()) < 550 ) {
 		$('a.next-project').html('&#8594');
 		$('a.prev-project').html('&#8592'); 
@@ -78,7 +74,7 @@ $(document).ready(function(){
 			$("nav").removeClass("antra");	
 		}
 		
-		 //change class of nav img so it can come out of nav and get back in
+		//change class of nav img so it can come out of nav and get back in
 		if ( $window.scrollTop() >= distance - 500 ) {
 			// Your div has reached  top 130px
 			$(".beeldmerk").removeClass("in-nav");
@@ -163,6 +159,7 @@ $(document).ready(function(){
 				if (e.target.id == 'el') return;
 				e.preventDefault();
 		});
+//		Zorg dat bij schalen de container altijd in view blijft
 		$( window ).resize(function() {
 			$('html, body').animate({
 				scrollTop: $("#works-container").offset().top
@@ -180,6 +177,7 @@ $(document).ready(function(){
 				if (e.target.id == 'el') return;
 				e.preventDefault();
 		});
+//		Zorg dat bij schalen de container altijd in view blijft
 		$( window ).resize(function() {
 			$('html, body').animate({
 				scrollTop: $("#profile-container").offset().top
@@ -207,6 +205,8 @@ $(document).ready(function(){
 		$('body').removeClass('no-overflow');
 		$('body').unbind('touchmove');
 		$('body').unbind('mousewheel');
+		
+//		Zorg dat bij schalen de container altijd in view blijft
 		$( window ).resize(function() {
 			$('html, body').animate({
 				scrollTop: $("#works-container").offset().top
@@ -433,17 +433,6 @@ $(document).on('click','#work-load', function() {
 	return false;
 });
 
-//	Load next or previous page
-//$(document).on('click','#page-one', function() {
-//	$('#dynamic-container').load('inc/works-overzicht.php');
-//	return false;
-//});
-//
-//$(document).on('click','#page-two', function() {
-//	$('#dynamic-container').load('inc/works-overzicht-2.php');
-//	return false;
-//});
-
 // Load original content
 $(document).on('click','.back', function(){
 	// change background color of main container (in dept effect)
@@ -535,7 +524,7 @@ $(document).on('click','.back-to-overzicht', function(){
 	return false;
 })
 
-// Load and unload how-we-work.php
+// Load how-we-work.php
 $(document).on('click','#how-we-work', function() {
 	$('#dynamic-container-profile').animate({ left : '-100%' }, 300).queue(function( nxt ) {
 		$(this).css({ left: '100%'})
@@ -554,6 +543,7 @@ $(document).on('click','#how-we-work', function() {
 	return false;
 });
 
+// Unload how-we-work.php (Load original content)
 $(document).on('click','.back-to-profile', function(){
     $('#dynamic-container-profile').animate({ left : '100%' }, 300).queue(function( nxt ) {
 		$(this).load('inc/works-content.php .profile');
@@ -574,6 +564,8 @@ $(document).on('click','.back-to-profile', function(){
 
 //Filter functie-------------------------------------------
 // Filter different projects
+
+// Page 1
 $(document).on('click','#all1', function() {
 	$('#results .clickable').fadeOut(0);
 	$('#results .clickable').filter('.all1').fadeIn(500);
@@ -583,6 +575,7 @@ $(document).on('click','#all1', function() {
 	return false;
 });
 
+// page 2
 $(document).on('click','#all2', function() {
 	$('#results .clickable').fadeOut(0);
 	$('#results .clickable').filter('.all2').fadeIn( 500);
@@ -592,9 +585,8 @@ $(document).on('click','#all2', function() {
 	return false;
 });
 
+//Stores
 $(document).on('click','#stores', function() {
-// 	$('#results .clickable').fadeTo( 200 , 0.1);
-//	$('#results .clickable').filter('.stores').fadeTo( 200 , 1);
 	$('#results .clickable').fadeOut(0);
 	$('#results .clickable').filter('.stores').fadeIn( 500);
 	$('.stores').css({display:'inline-block'});
@@ -603,6 +595,7 @@ $(document).on('click','#stores', function() {
 	return false;
 });
 
+//Comnmercials
 $(document).on('click','#commercials', function() {
 	$('#results .clickable').fadeOut(0);
 	$('#results .clickable').filter('.commercials').fadeIn( 500);
@@ -612,9 +605,8 @@ $(document).on('click','#commercials', function() {
 	return false;
 });
 
+//Interiors
 $(document).on('click','#interiors', function() {
-	//$('#results .clickable').fadeTo( 200 , 0.1);
-	//$('#results .clickable').filter('.interiors').fadeTo( 200 , 1);
 	$('#results .clickable').fadeOut(0);
 	$('#results .clickable').filter('.interiors').fadeIn( 500);
 	$('.interiors').css({display:'inline-block'});
@@ -623,9 +615,8 @@ $(document).on('click','#interiors', function() {
 	return false;
 });
 
+//Horeca
 $(document).on('click','#horeca', function() {
-	//$('#results .clickable').fadeTo( 200 , 0.1);
-	//$('#results .clickable').filter('.horeca').fadeTo( 200 , 1);
 	$('#results .clickable').fadeOut(0);
 	$('#results .clickable').filter('.horeca').fadeIn( 500);
 	$('.horeca').css({display:'inline-block'});
@@ -634,9 +625,8 @@ $(document).on('click','#horeca', function() {
 	return false;
 });
 
+//Stands
 $(document).on('click','#stands', function() {
-	//$('#results .clickable').fadeTo( 200 , 0.1);
-	//$('#results .clickable').filter('.stands').fadeTo( 200 , 1);
 	$('#results .clickable').fadeOut(0);
 	$('#results .clickable').filter('.stands').fadeIn( 500);
 	$('.stands').css({display:'inline-block'});
@@ -645,9 +635,8 @@ $(document).on('click','#stands', function() {
 	return false;
 });
 
+//Designs
 $(document).on('click','#designs', function() {
-	//$('#results .clickable').fadeTo( 200 , 0.1);
-	//$('#results .clickable').filter('.designs').fadeTo( 200 , 1);
 	$('#results .clickable').fadeOut(0);
 	$('#results .clickable').filter('.designs').fadeIn( 500);
 	$('.designs').css({display:'inline-block'});
@@ -656,6 +645,7 @@ $(document).on('click','#designs', function() {
 	return false;
 });
 
+// Initiate preload function
 if ( ($(window).width()) > 900 ) {
 	$('a, img, body, #een, #twee, #drie, #vier, #vijf, #zes, #works-container, #profile-container').addClass('hires');
 	if (jQuery.browser.mobile == false) {
@@ -667,7 +657,7 @@ if ( ($(window).width()) > 900 ) {
 
 //on window resize check for widths
 $( window ).resize(function() {
-	//check width for hires img
+	//check width for hires img (initiate preload)
 	if ( ($(window).width()) > 900 ) {
 		$('a, img, body, #een, #twee, #drie, #vier, #vijf, #zes, #works-container, #profile-container').addClass('hires');
 		if (jQuery.browser.mobile == false) {
@@ -676,6 +666,7 @@ $( window ).resize(function() {
 	}else{
 		$('a, img, body, #een, #twee, #drie, #vier, #vijf, #zes, #works-container, #profile-container').removeClass('hires');
 	};
+//	Change content of next & prev projectbuttons
 	if ( ($(window).width()) < 550 ) {
 		$('a.next-project').html('&#8594');
 		$('a.prev-project').html('&#8592'); 
