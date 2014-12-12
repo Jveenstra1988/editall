2c6f1744308bf8075036cdb5135e5818d4387ed1
jeroenklaver <jeroenklaver1987@gmail.com>
2014-12-12 16:48:55 +0100

    zijnavigatie styling projecten

--- inc/works-overzicht.php
+++ inc/works-overzicht.php
@@ -3,11 +3,11 @@
 
 <header>
 
-<a href="#" class="btn-rood back">&#8592; Terug</a>
-<h1 class="titel-wit titel-left">Projecten</h1>
+<a href="#" class="btn-wit back">&#8592; Terug</a>
+<h1 class="txt-wit">Projecten</h1>
 
                         <div class="subnav">
-                            <a href="#" class="btn-rood active proj" id="all"> All</a>
+                            <a href="#" class="btn-wit stroke cat active" id="all"> All</a>
                             <a href="#" class="btn-wit stroke cat" id="stores">Stores</a>
                             <a href="#" class="btn-wit stroke cat" id="commercials">Commercials</a>
                             <a href="#" class="btn-wit stroke cat" id="interiors">Interieurs</a>
