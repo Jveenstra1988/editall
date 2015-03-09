f7aa82be405e8ee3c2078246623331cda3e62efd
Jeroen Klaver <jeroen@destoeptegel.nl>
2015-03-09 14:25:14 +0100

    algemene voorwaarden
    Pagina toevoeging algemene voorwaarden

---
+++ av.php
@@ -0,0 +1,314 @@
+<?php require 'inc/header_lite.php'; ?>
+
+<section class="bg-wit clear av">
+	<a target="_blank" download="algemenevoorwaarden_editall_2015.pdf" href="algemenevoorwaarden_editall_2015.pdf" class="btn-wit">
+	downloaden</a>
+
+	<header>
+	<img src="img/logo.png" class="avlogo">
+	</header>
+	</section>
+
+<!--Content - PROJECTS-->
+<section class="av" class="bg-wit">
+    <div id="">
+    <article class="txt-zwart">
+    	<h1 class="txt-zwart">Algemene voorwaarden</h1>
+    <span class="bg-rood"></span>
+
+	<h5 class="">Artikel 1: Toepasselijkheid</h5>
+	<p>
+		Deze algemene voorwaarden zijn van toepassing op alle door Edit-all opgestelde aanbiedingen en tussen Edit-All en de
+		opdrachtgever gesloten overeenkomsten. Zij zijn eveneens van toepassing op alle verbintenissen die voortvloeien uit
+		nadien tussen partijen gesloten overeenkomsten. Toepassing van door de opdrachtgever ingeroepen algemene
+		voorwaarden worden uitdrukkelijk van de hand gewezen. Afwijkingen van deze algemene voorwaarden gelden slechts
+		indien zij schriftelijk zijn overeengekomen met Edit-All.
+	</p>
+	<p>
+		Indien &eacute;&eacute;n of meer bepalingen van deze algemene voorwaarden of van overeenkomsten waarop deze algemene
+		voorwaarden van toepassing zijn, nietig of niet-rechtsgeldig geacht worden, zullen de overige voorwaarden van kracht
+		blijven.
+	</p>
+		<h5>Artikel 2: Definities</h5>
+	<p>
+		Onder opdrachtgever wordt verstaan degene die opdracht voor de werkzaamheden heeft verstrekt. Edit-All is
+		opdrachtnemer.
+	</p>
+	<p>
+		Onder opdracht wordt verstaan het verzoek van de opdrachtgever aan Edit-All om tegen betaling werkzaamheden te
+		verrichten.
+	</p>
+	<p>
+		Onder werkzaamheden wordt verstaan al datgene dat Edit-All ten behoeve van opdrachtgever, binnen het kader van
+		de verstrekte opdracht, maakt en of levert.
+	</p>
+	<p>
+		Onder werk wordt verstaan het product dat door Edit-All vervaardigd is c.q. geleverd wordt.
+	</p>
+	
+	<h5>Artikel 3: Aanbod/offerte</h5>
+	<p>
+		Alle aanbiedingen zullen vrijblijvend zijn, tenzij in de aanbieding zelf anders is vermeld. Alle aanbiedingen worden gedaan
+		onder het voorbehoud van prijsveranderingen en andere veranderde omstandigheden welke de inhoud van de
+		voorwaarden van de overeenkomsten beïnvloeden. In al deze omstandigheden zal Edit-All gerechtigd zijn de prijzen te
+		verhogen in overeenstemming met de veranderde omstandigheden.
+		Overschrijdingen van aanbiedingen tot 10% worden als begrotingsrisico door opdrachtgever geaccepteerd en hoeven
+		dan ook als zodanig niet te worden gemeld door Edit-All.
+	</p>
+	<p>
+		De van het aanbod deel uitmakende documenten (zoals tekeningen, beschrijvingen e.d.) zijn zo nauwkeurig mogelijk,
+		doch zijn niet bindend en blijven het (intellectuele) eigendom van Edit-All. Zij mogen zonder toestemming van Edit-All
+		niet worden gebruikt, gekopieerd, of aan derden ter hand worden gesteld of op andere wijze openbaar worden
+		gemaakt.
+	</p>
+	<p>
+		Indien de opdrachtgever het aanbod niet aanvaardt, is hij verplicht terstond alle in het vorige artikellid genoemde
+		gegevens terug te geven aan Edit-All.
+	</p>
+	<p>
+		Edit-All is gerechtigd de kosten die gemoeid zijn met het aanbod in rekening te brengen, mits de opdrachtgever vooraf
+		schriftelijk op deze kosten is gewezen.
+	</p>
+	<p>
+		Indien zich omstandigheden voordoen die bij het sluiten van de overeenkomst niet voorzienbaar waren en de
+		nakoming van de overeenkomst substantieel beïnvloeden zullen partijen in gezamenlijk overleg en naar redelijkheid en
+		billijkheid een oplossing vinden die recht doet aan de de belangen van beide partijen in het kader van deze
+		overeenkomst.
+	</p>
+	
+	<h5>Artikel 4: Overeenkomst</h5>
+		<ul>
+		<p>Indien het aanbod van Edit-All wordt aanvaard, dan komt de overeenkomst eerst tot stand op het moment dat Edit-All:</p>
+			<li>hetzij de aanvaarding binnen een redelijke termijn bevestigt;</li>
+			<li>hetzij met de uitvoering van de opdracht aanvangt.</li>
+		</ul>
+	<p>
+		De opdrachtbevestiging wordt geacht de overeenkomst juist en volledig weer te geven.
+		Eventueel later gemaakte aanvullende afspraken en/of wijzigingen binden Edit-All slechts indien deze door Edit-All
+		schriftelijk zijn bevestigd.
+	</p>
+	<p>
+		Voor werkzaamheden waarvoor naar hun aard en omvang geen offerte c.q. opdrachtbevestiging wordt verzonden,
+		wordt de factuur tevens als opdrachtbevestiging beschouwd. Ook de factuur wordt geacht de overeenkomst juiste en
+		volledig weer te geven. Elke overeenkomst wordt aan de zijde van Edit-All aangegaan onder de opschortende
+		voorwaarde dat de wederpartij, uitsluitend ter beoordeling van Edit-All, voldoende kredietwaardig blijkt voor de
+		geldelijke nakoming van de overeenkomst.
+	</p>
+	<p>
+		Edit-All is gerechtigd om bij of na het aangaan van de overeenkomst, alvorens (verder) te presteren, van de
+		opdrachtgever te eisen dat deze een deelbetaling zal doen. Edit-All is eveneens gerechtigd zekerheid te vragen dat aan
+		de overige verplichtingen zal worden voldaan.
+	</p>
+	<h5>Artikel 5: Oplevering en levertijd</h5>
+	<p>
+		Edit-All zal de overeengekomen levertijd zoveel mogelijk trachten te realiseren. Tenzij uitdrukkelijk schriftelijk anders is
+		overeengekomen, neemt Edit-All ten aanzien van de afgesproken leveringstermijn geen enkele garantie op zich en geeft
+		niet-tijdige levering opdrachtgever geen recht op schadevergoeding, ontbinding van de overeenkomst of niet-nakoming
+		van enige verplichting van opdrachtgever jegens Edit-All. Bij overschrijding van de deze levertijd zal Edit-All in overleg
+		treden met de opdrachtgever. Edit-All zal proberen dit overleg zo spoedig te laten plaatsvinden.
+	</p>
+	<ul>
+		<p>Het werk wordt als opgeleverd beschouwd:</p>
+		<li>wanneer Edit-All aan de opdrachtgever kennis heeft gegeven dat het werk is voltooid en opdrachtgever het werk
+			heeft goedgekeurd, dan wel aanvaard;</li>
+		<li>wanneer uiterlijk 8 dagen zijn verstreken nadat Edit-All schriftelijk aan de opdrachtgever heeft verklaard dat het werk
+			is voltooid en deze heeft nagelaten het werk binnen die termijn goed te keuren, dan wel te aanvaarden;</li>
+		<li>wanneer de opdrachtgever het werk (vroegtijdig) in gebruik neemt, met dien verstande dat door de (vroegtijdige)
+			ingebruikneming van een gedeelte van het werk, dat gedeelte als opgeleverd wordt beschouwd.</li>
+		<li>Kleine gebreken die binnen afzienbare termijn na aflevering kunnen worden hersteld en 
+			die het functioneren van het geleverde niet beïnvloeden, zullen aan de oplevering niet in de weg staan.</li>
+	</ul>
+	
+	<p>
+		Oplevering ontslaat Edit-All van alle aansprakelijkheid voor gebreken die de opdrachtgever op dat tijdstip redelijkerwijs
+		had moeten ontdekken. Als gevolg van de oplevering gaat het risico voor het geleverde over van Edit-All naar de
+		opdrachtgever.
+	</p>
+	
+	<p>
+		Bij eventuele tekorten of beschadigingen van het geleverde die bij aflevering aanwezig zijn, dient de opdrachtgever dit
+		bij aflevering te melden, bij gebreke waarvan de opdrachtgever geacht wordt het geleverde te hebben goedgekeurd.
+	</p>
+	
+	<h5>Artikel 6: Uitvoering van de overeenkomst</h5>
+	<p>
+		De opdrachtgever verplicht zich om aan Edit-All voorafgaand aan de uitvoering van het werk alle informatie en
+		gegevens te verstrekken die relevant kunnen zijn voor de uitvoering van de opdracht.
+	</p>
+	<p>
+		Edit-All zal opdrachtgever op de kortst mogelijke termijn informeren, telkens wanneer een door opdrachtgever
+		gewenste of noodzakelijke wijziging in de uitvoering van de overeenkomst leidt tot veranderingen in het
+		overeengekomen tijdstip van voltooiing, of de honorering.
+	</p>
+	<p>
+		Wijziging of aanvullingen in de uitvoering van de overeenkomst kan slechts na nadere overeenstemming tussen partijen
+		plaatsvinden; eventuele extra kosten die daaruit voortvloeien komen voor rekening van opdrachtgever. Tot meerwerk
+		worden niet gerekend additionele werkzaamheden die Edit-All had kunnen of moeten voorzien teneinde conform de
+		overeenkomst te presteren, in welke geval eventuele extra kosten voor rekening van Edit-All komen. Indien de
+		wijziging/aanvulling cineset ten gevolge heeft, zullen deze minderkosten ten gunste van opdrachtgever komen.
+		Eventuele intellectuele eigendomsrechten die voortvloeien uit de opdracht die Edit-All voor opdrachtgever uitvoert,
+		blijven bij Edit-All, tenzij partijen uitdrukkelijk schriftelijk anders overeenkomen.
+	</p>
+	
+	<h5>Artikel 7: Betaling</h5>
+	<p>
+		Betaling vindt plaats binnen veertien dagen na factuurdatum. Betalingen dienen te geschieden zonder aftrek,
+		compensatie of opschorting uit welke hoofde dan ook.
+	</p>
+	<p>
+		Indien opdrachtgever zijn verplichtingen tegenover Edit-All niet nakomt, in staat van faillissement wordt verklaard,
+		surs&eacute;ance van betaling aanvraagt, dan wel bij stillegging of liquidatie van onderneming van opdrachtgever, zal het door
+		opdrachtgever verschuldigde onmiddellijk en volledig opeisbaar zijn, zonder dat enige ingebrekestelling vereist is.
+	</p>
+	<p>
+		Indien opdrachtgever verschuldigde bedragen niet binnen de overeengekomen termijn betaalt, zal opdrachtgever,
+		zonder dat enige ingebrekestelling nodig is, een contractuele rente van 1,5% per maand verschuldigd zijn. Indien
+		opdrachtgever na ingebrekestelling nalatig blijft de vordering te voldoen, kan de vordering uit handen worden gegeven,
+		in welk geval opdrachtgever het alsdan verschuldigde totale bedrag, tevens gehouden is tot vergoeding van de volledige
+		buitengerechtelijke incassokosten. Deze kosten worden op 15% van het betrokken bedrag gefixeerd en zullen
+		tenminste €250,- bedragen.
+	</p>
+	
+	<ul>
+		<p>	Indien de opdrachtgever nalatig is aan eigen betalingsverplichting te voldoen, dan wel in gebreke blijft ten aanzien van
+		het stellen van nadere zekerheid zoals bedoeld in artikel 4 lid 5, heeft Edit-All het recht om, naar haar keuze:</p>
+	
+		<li>levering en/of werkzaamheden op de schorten;</li>
+		<li>de overeenkomst zonder rechterlijke tussenkomst te ontbinden.In de hierboven omschreven gevallen van
+			opschorting of ontbinding, is Edit-All niet tot enige schadevergoeding gehouden. Opschorting of ontbinding brengt
+			geen wijziging in de betalingsverplichting van opdrachtgever.</li>
+	</ul>
+	</p>
+	
+	<h5>Artikel 8: Annulering</h5>
+	<p>
+		Indien opdrachtgever de opdracht geheel of gedeeltelijk annuleert, is hij gehouden aan Edit-All alle met het oog op de
+		uitvoering van de opdracht redelijkerwijs gemaakte kosten te vergoeden en, indien Edit-All zulks wenst, voor de
+		uitvoering van e opdracht bestemde materialen voor zijn rekening te nemen, tegen de door Edit-All in haar calculatie
+		opgenomen prijzen, onverminderd het recht van Edit-All op vergoeding wegens winstderving, en van overige uit de
+		annulering voortvloeiende schaden.
+	</p>
+	<p>
+		<strong>Artikel 9: Reclames</strong>
+		Terzake aantoonbare gebreken moet opdrachtgever, nadat Edit-All de door haar uitgevoerde opdracht aan
+		opdrachtgever heeft geleverd of voorgelegd, binnen acht dagen na het leveren schriftelijk reclameren.
+	</p>
+	<p>
+		Reclames omtrent facturen moet binnen acht dagen na factuurdatum schriftelijk ter kennis van Edit-All worden
+		gebracht. Hierdoor wordt de betalingstermijn niet opgeschort.
+	</p>
+	<p>
+		Na het verstrijken van bovengenoemde termijnen wordt de opdrachtgever geacht het geleverde, respectievelijk de
+		factuur te hebben goedgekeurd. Alsdan worden reclames niet meer door Edit-All in behandeling genomen.
+		Retournering van het geleverde kan slechts geschieden na voorafgaande schriftelijke toestemming van Edit-All, onder
+		door Edit-All te bepalen voorwaarden.
+	</p>
+	<p>
+		Behoudens tegenbewijs, zijn de de gegevens uit de administratie van Edit-All beslissend.
+	</p>
+	
+	<h5>Artikel 10: Opschorting en ontbinding</h5>
+	<p>
+		Indien opdrachtgever niet, niet-behoorlijk of niet-tijdig voldoet aan enige verplichting voortvloeiend uit de
+		overeenkomst, als mede in geval van faillissement, surs&eacute;ance van betaling, stillegging of liquidatie van het bedrijf
+		van de opdrachtgever, wordt hij geacht van rechtswege in verzuim te zijn en heeft Edit-All het recht zonder enige
+		ingebrekestelling en zonder rechterlijke tussenkomst de uitvoering van de overeenkomst op te schorten of de
+		overeenkomst geheel of gedeeltelijk te ontbinden, ter hare keuze, zonder dat zij tot enige schadevergoeding of garantie
+		gehouden is, doch onverminderd haar verdere toekomende rechten. In deze gevallen is elke vordering die Edit-All ten
+		laste van de opdrachtgever heeft of krijgt, dadelijk en ineens opeisbaar.
+	</p>
+
+	<h5>Artikel 11: Aansprakelijkheid</h5>
+	<p>		
+		Edit-All kan op geen enkele wijze aansprakelijk worden gesteld door opdrachtgever en/of derden voor schaden van
+		welke aard dan ook, voortvloeiende uit of in verband met door Edit-All krachtens de overeenkomst geleverde
+		producten en/of diensten.
+	</p>
+	<p>
+		Indien er voor zover enige aansprakelijkheid van Edit-All wordt vastgesteld, zal de aansprakelijkheid van Edit-All
+		beperkt zijn tot een vergoeding van ten hoogste de factuurwaarde van het geleverde. Tot verdere schadevergoeding is
+		Edit-All nimmer verplicht.
+	</p>
+	<h5>Artikel 12: Overmacht</h5>
+	<p>
+		In geval Edit-All door overmacht wordt verhinderd de opdracht geheel of gedeeltelijk uit te voeren, heeft Edit-All het
+		recht om zonder gerechtelijke tussenkomst de uitvoering van de overeenkomst op te schorten of de overeenkomst
+		geheel of gedeeltelijk als ontbonden te beschouwen, zulks ter hare keuze, zonder dat Edit-All tot enige
+		schadevergoeding of garantie is gehouden.
+	</p>
+	<p>
+	Onder overmacht wordt mede verstaan: staking, uitsluiting, brand, machinebreuk en andere bedrijfsstoornissen, hetzij
+	bij Edit-All, hetzij bij haar leveranciers van goederen en diensten, transportstoringen, overmatig ziekteverzuim van
+	personeel en andere buiten haar macht liggende gebeurtenissen, zoals oorlog, blokkade, oproer, epidemie, devaluatie,
+	natuurrampen, alsmede plotselinge verhogingen van heffingen door de overheid opgelegd, vertragingen of uitblijven van
+	leveringen door leveranciers, en het niet verkrijgen van benodigde vergunningen.
+	</p>
+	<p>
+	Indien naar het oordeel van Edit-All de overmacht van tijdelijke aard zal zijn, heeft zij het recht de uitvoering van de
+	overeenkomst zo lang op te schorten totdat de omstandigheid die de overmacht oplevert zich niet meer voordoet.
+	Is naar het oordeel van Edit-All de overmachtsituatie van blijvende aard, dan kunnen partijen een regeling treffen over
+	de ontbinding van de overeenkomst en de daaraan verbonden gevolgen.
+	</p>
+	<p>
+	Edit-All is gerechtigd betaling te vorderen van de prestaties die bij de uitvoering van de desbetreffende opdracht zijn
+	verricht, voordat van de overmacht veroorzakende omstandigheid is gebleken.
+	</p>
+	<p>
+	Edit-All heeft het recht zich ook op overmacht te beroepen, indien de omstandigheid die de overmacht oplevert
+	intreedt, nadat haar prestatie geleverd had moeten zijn.
+	</p>
+	
+	<h5>Artikel 13: Eigendomsvoorbehoud</h5>
+	<p>
+		Alle door Edit-All geleverde goederen blijven, tot op het moment van volledige betaling van al hetgeen opdrachtgever
+		uit welke hoofde ook jegens Edit-All verschuldigd is, het eigendom van Edit-All. Onder verschuldigdheid wordt tevens
+		begrepen dat de toekomstige vorderingen op de opdrachtgever, met inbegrip van rente en kosten.
+	</p>
+	
+	<h5>Artikel 14: Toepasselijk recht</h5>
+	<p>
+		Deze algemene voorwaarden en de overeenkomst waarop deze voorwaarden van toepassing zijn, worden beheerst
+		door Nederlands recht.
+	</p>
+	<p>
+		Elke geschil betreffende deze algemene voorwaarden en de overeenkomsten waarop deze voorwaarden van toepassing
+		zijn, alsmede ieder ander geschil terzake van of in verband hiermee, zal door bevoegde rechter te Amsterdam worden
+		beslecht.
+	</p>
+	<p>
+		Geschillen zullen onmiddellijk na het ontstaan ervan ter kennis van de wederpartij gebracht worden. Er is sprake van
+		een geschil indien &eacute;&eacute;n der partijen een geschil aanwezig acht.
+	</p>
+	<p>
+		Alvorens een geschil aan de rechter ter beoordeling wordt voorgelegd, zullen partijen trachten in der minne een
+		oplossing voor het gerezen verschil te vinden.
+	</p>
+</article>
+
+<article>
+	<a target="_blank" download="algemenevoorwaarden_editall_2015.pdf" href="algemenevoorwaarden_editall_2015.pdf" class="btn-grijs">
+	Algemene voorwaarden pdf</a>
+</article>
+
+
+<article>
+	<ul class="adres">
+
+	<p class="adres">
+	<img src="img/logo-zwart.png" class="big">
+	<span style="display:none;">Edit-All B.V.</span>
+
+
+		<li>Samsonweg 134</li>
+		<li>1521 RM Wormerveer</li>
+		<li><a style="text-decoration:underline; color:#D41217;" href="tel:+310756283281">075 - 628 32 81</a></li>
+		<br />
+		<li>
+			<b style="display:block;">Bank/rekeningummer:</b>NL78 RABO 0174 186401</li>
+		<li><b style="display:block;">K.v.K.:</b>56691459</li>
+		<li><b style="display:block;">BTW nr.:</b> 8522 65 074 B01</li>
+	</ul>
+	</p>
+</article>
+
+</section>
